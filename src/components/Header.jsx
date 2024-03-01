0396906b5ede7f144ac24b825c92e476bb3a287e
Bilal-Raza7 <bilal.7897.raza@gmail.com>
2024-03-01 19:04:44 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-md dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
                 <a href="/">Homepage</a>
